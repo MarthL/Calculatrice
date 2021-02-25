2ba524c1e5906b08a0d4483a538d19d90d3670c0
Martin Aubertin <martin.aubertin@gmail.com>
2021-02-25 15:43:51 +0100

    Préparation front     ajout des boutons     préparation des emplacements

--- index.php
+++ index.php
@@ -10,64 +10,67 @@
 </head>
 
 <body>
-    <div class="calculette">
-        <div class="ecran">
-            <p id="ecranUtilisateur">
+    <div class="box">
+        <div class="calculette">
+            <div class="ecran">
+                <p id="ecranUtilisateur">
+                    hello
+                </p>
+            </div>
+            <div class="btn" id="i1">
+                <p>1</p>
+            </div>
+            <div class="btn" id="i2">
+                <p>2</p>
+            </div>
+            <div class="btn" id="i3">
+                <p>3</p>
+            </div>
+            <div class="btn" id="i4">
+                <p>4</p>
+            </div>
+            <div class="btn" id="i5">
+                <p>5</p>
+            </div>
+            <div class="btn" id="i6">
+                <p>6</p>
+            </div>
+            <div class="btn" id="i7">
+                <p>7</p>
+            </div>
+            <div class="btn" id="i8">
+                <p>8</p>
+            </div>
+            <div class="btn" id="i9">
+                <p>9</p>
+            </div>
+            <div class="btn" id="i0">
+                <p>0</p>
+            </div>
+            <div class="btn" id="virgule">
+                <p>.</p>
+            </div>
+            <div class="btn" id="more">
+                <p>+</p>
+            </div>
+            <div class="btn" id="less">
+                <p>-</p>
+            </div>
+            <div class="btn" id="divise">
+                <p>/</p>
+            </div>
+            <div class="btn" id="multiplication">
+                <p>*</p>
+            </div>
+            <div class="btn" id="delete">
+                <p>c</p>
+            </div>
+            <div class="btn" id="result">
+                <p>=</p>
+            </div>
+        </div>
 
-                hello
-            </p>
-        </div>
-        <div class="btn" id="i1">
-            <p>1</p>
-        </div>
-        <div class="btn" id="i2">
-            <p>2</p>
-        </div>
-        <div class="btn" id="i3">
-            <p>3</p>
-        </div>
-        <div class="btn" id="i4">
-            <p>4</p>
-        </div>
-        <div class="btn" id="i5">
-            <p>5</p>
-        </div>
-        <div class="btn" id="i6">
-            <p>6</p>
-        </div>
-        <div class="btn" id="i7">
-            <p>7</p>
-        </div>
-        <div class="btn" id="i8">
-            <p>8</p>
-        </div>
-        <div class="btn" id="i9">
-            <p>9</p>
-        </div>
-        <div class="btn" id="i0">
-            <p>0</p>
-        </div>
-        <div class="btn" id="more">
-            <p>+</p>
-        </div>
-        <div class="btn" id="less">
-            <p>-</p>
-        </div>
-        <div class="btn" id="divise">
-            <p>/</p>
-        </div>
-        <div class="btn" id="mutiplication">
-            <p>*</p>
-        </div>
-        <div class="btn" id="delete">
-            <p>c</p>
-        </div>
-        <div class="btn" id="result">
-            <p>=</p>
-        </div>
     </div>
-
-
     <script src="./main.js"></script>
 </body>
 
